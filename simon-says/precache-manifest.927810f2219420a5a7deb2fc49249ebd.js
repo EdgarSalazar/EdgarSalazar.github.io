91debeb01a206c9215e89ba383fb313f65635b02
Edgar Froes <edgar.froes@gmail.com>
2019-06-23 01:17:58 -0300

    Simon says (with prank)

---
+++ simon-says/precache-manifest.927810f2219420a5a7deb2fc49249ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c37603b374741a0b952e88476722706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5359bb2655a558ab51",
+    "url": "/static/css/main.4b95cc14.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "6c5359bb2655a558ab51",
+    "url": "/static/js/main.fc54dd15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56fd51febe71d9512f49906f2f2fde3f",
+    "url": "/static/media/lose.56fd51fe.mp3"
+  }
+]);
