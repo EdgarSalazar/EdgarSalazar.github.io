6b53b5345248d21a65058bcd262373c34a99eff5
Edgar Froes <edgar.froes@gmail.com>
2019-06-24 14:07:51 -0300

    Simon says on home just to test google ads

---
+++ precache-manifest.b6e5cad3f2688b34715c06a8d9a5405b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627b25fd2b194e55aabc8a26685aa98f",
+    "url": "/simon-says/index.html"
+  },
+  {
+    "revision": "ab2f50dd6e3d3681c78d",
+    "url": "/simon-says/static/css/main.a7e1a8f7.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/simon-says/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "ab2f50dd6e3d3681c78d",
+    "url": "/simon-says/static/js/main.b28f1311.chunk.js"
+  },
+  {
+    "revision": "2ff488b8d7e0b2971c4e",
+    "url": "/simon-says/static/js/runtime~main.4578f539.js"
+  },
+  {
+    "revision": "56fd51febe71d9512f49906f2f2fde3f",
+    "url": "/simon-says/static/media/lose.56fd51fe.mp3"
+  }
+]);
