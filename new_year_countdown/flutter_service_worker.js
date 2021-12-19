dbd595acdb55be1bf06a764d0b4ba665406d2221
Edgar Froes <certsys.eleal@bv.com.br>
2021-12-19 03:09:11 -0300

    update href

--- new_year_countdown/flutter_service_worker.js
+++ new_year_countdown/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6e8f566f734e1a2b01a85048d8c291aa",
-"index.html": "7e513a6151b2dae78e7fa2ef63a4c598",
-"/": "7e513a6151b2dae78e7fa2ef63a4c598",
+"index.html": "ba4af117b404a978aa7fca9b8972dce8",
+"/": "ba4af117b404a978aa7fca9b8972dce8",
 "main.dart.js": "aa141c8754652f085447455ff564b01e",
 "icons/favicon-16x16.png": "8377082132ba92624a084a7da4dbc11b",
 "icons/favicon.ico": "1856ace050cf694b8ff5388ddae93ce6",
