c894136cd5ad99fe42c35c7482f30fd550d81c92
Edgar Froes <certsys.eleal@bv.com.br>
2021-12-19 03:31:37 -0300

    rebuild

--- new_year_countdown/flutter_service_worker.js
+++ new_year_countdown/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6e8f566f734e1a2b01a85048d8c291aa",
-"index.html": "abb98a8791d57d364e26b7bac7761818",
-"/": "abb98a8791d57d364e26b7bac7761818",
+"index.html": "43afe926e8973e8be82bd03fe4b63e1f",
+"/": "43afe926e8973e8be82bd03fe4b63e1f",
 "main.dart.js": "3be56cf05f745f26b597b5953516f477",
 "icons/favicon-16x16.png": "8377082132ba92624a084a7da4dbc11b",
 "icons/favicon.ico": "1856ace050cf694b8ff5388ddae93ce6",
@@ -41,11 +41,7 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/assets/flags/brazil.svg": "959af1e7a2d161e0fa1210d2c16af577",
 "assets/assets/flags/united-states.svg": "aefba3fef673e14305b112b0e82805fe",
-"assets/assets/fireworks-particle.png": "db9cd9d0f841a501771c67f1c75bbc72",
-"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
-"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
-"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498"
+"assets/assets/fireworks-particle.png": "db9cd9d0f841a501771c67f1c75bbc72"
 };
 
 // The application shell files that are downloaded before a service worker can
